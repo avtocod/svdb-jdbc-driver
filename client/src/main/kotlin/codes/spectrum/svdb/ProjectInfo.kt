500d82e522b61e340979c1220fa3a68176972745
Степан Геккель <gekkel.s@spectrumdata.ru>
2024-11-06 13:52:59 +0500

    up maxbound of grpc message

--- client/src/main/kotlin/codes/spectrum/svdb/ProjectInfo.kt
+++ client/src/main/kotlin/codes/spectrum/svdb/ProjectInfo.kt
@@ -1,5 +1,5 @@
 package codes.spectrum.svdb
 
 object ProjectInfo {
-    val version = "2.0.30" /* значение автоматически обновляется при сборке */
+    val version = "2.1.8" /* значение автоматически обновляется при сборке */
 }
